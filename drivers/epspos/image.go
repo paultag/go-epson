007f1a43981a1a2abbf00b231d3e8399d48ec912
Paul Tagliamonte <tag@pault.ag>
2016-10-10 10:36:19 -0400

    add in a workaround for overlarge images

--- drivers/epspos/image.go
+++ drivers/epspos/image.go
@@ -10,7 +10,45 @@ func uint16Touint8s(x uint16) (uint8, uint8) {
 	return uint8(x & 0xFF), uint8(x >> 8 & 0xFF)
 }
 
+func graySubImage(p image.Gray, r image.Rectangle) image.Gray {
+	r = r.Intersect(p.Rect)
+	if r.Empty() {
+		return image.Gray{}
+	}
+	i := p.PixOffset(r.Min.X, r.Min.Y)
+	return image.Gray{
+		Pix:    p.Pix[i:],
+		Stride: p.Stride,
+		Rect:   r,
+	}
+}
+
 func (e EPSPOS) PrintImage(img image.Gray) error {
+	bounds := img.Bounds()
+	if bounds.Max.X != 512 {
+		return fmt.Errorf("Please pass 512px wide images. This is buggy behavior. Complain to Paul.")
+	}
+
+	if bounds.Max.Y > 1024 {
+		/* If it's an overlarge image, let's split it and recurse */
+
+		if err := e.printImage(graySubImage(img, image.Rectangle{
+			Min: image.Point{X: 0, Y: 0},
+			Max: image.Point{X: 512, Y: 1024},
+		})); err != nil {
+			return err
+		}
+		return e.PrintImage(graySubImage(img, image.Rectangle{
+			Min: image.Point{X: 0, Y: 1024},
+			Max: bounds.Max,
+		}))
+	}
+
+	/* Otherwise, let's just rock and roll on this sucka */
+	return e.printImage(img)
+}
+
+func (e EPSPOS) printImage(img image.Gray) error {
 	bounds := img.Bounds()
 
 	if bounds.Max.X > math.MaxUint16 || bounds.Max.Y > math.MaxUint16 {
@@ -20,6 +58,23 @@ func (e EPSPOS) PrintImage(img image.Gray) error {
 	var width uint16 = uint16(bounds.Max.X)
 	var height uint16 = uint16(bounds.Max.Y)
 	var widthBytes uint16 = ((width + 7) >> 3)
+
+	if height >= 1024 {
+		/* XXX: This is actually wrong, and someone's going to stub their toe
+		*       on this one, likely me. So, future Paul, here's what's up here:
+		*
+		*       The issue is that the underlying Epson device hates things that
+		*       are larger than some limit. It's not totally clear to me what
+		*       that limit is, since the uint16 would be the natural limit.
+		*
+		*       However, the printer (when it gets super long images) will start
+		*       to overrun and print nonsense. It may be that it stores
+		*       the widthBytes * height in a uint16, causing overflows, but
+		*       that also seems unlikely. In the meantime, I'm going to assume
+		*       512px images are always passed, and hardcode bad lengths
+		*       until I can go in and figure out exactly what's going on here. */
+		return fmt.Errorf("Image is too long - can't do more than 1024")
+	}
 
 	w1, w2 := uint16Touint8s(widthBytes)
 	h1, h2 := uint16Touint8s(height)
