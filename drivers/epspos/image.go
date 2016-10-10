b8c13aefefa8ffb02d63d4905a5f4fcfaa936da6
Paul Tagliamonte <tag@pault.ag>
2016-10-10 13:35:06 -0400

    Reflow rect to be multiple chunks

--- drivers/epspos/image.go
+++ drivers/epspos/image.go
@@ -3,49 +3,106 @@ package epspos
 import (
 	"fmt"
 	"image"
-	"math"
+	"io"
+	// "math"
 )
 
 func uint16Touint8s(x uint16) (uint8, uint8) {
 	return uint8(x & 0xFF), uint8(x >> 8 & 0xFF)
 }
 
-func (e EPSPOS) PrintImage(img image.Gray) error {
-	bounds := img.Bounds()
+type EPSPOSBitmap struct {
+	height     uint16
+	widthBytes uint16
+	image      []byte
+}
 
-	if bounds.Max.X > math.MaxUint16 || bounds.Max.Y > math.MaxUint16 {
-		return fmt.Errorf("Image is too big for a uint16")
-	}
-
-	var width uint16 = uint16(bounds.Max.X)
-	var height uint16 = uint16(bounds.Max.Y)
+func NewEPSPOSBitmapFromGray(img image.Gray, r image.Rectangle) (*EPSPOSBitmap, error) {
+	width := uint16(r.Max.X - r.Min.X)
+	height := uint16(r.Max.Y - r.Min.Y)
 	var widthBytes uint16 = ((width + 7) >> 3)
 
-	w1, w2 := uint16Touint8s(widthBytes)
-	h1, h2 := uint16Touint8s(height)
+	bitmap := EPSPOSBitmap{height: height, widthBytes: widthBytes}
+	bitmap.image = make([]byte, bitmap.Size())
+	return &bitmap, bitmap.copyFromGray(img, r)
+}
 
-	imageBufferSize := (uint32(widthBytes) * uint32(height))
-	imageBuffer := make([]byte, imageBufferSize)
+/* Copy a chunk of a grey image in */
+func (e EPSPOSBitmap) copyFromGray(img image.Gray, r image.Rectangle) error {
+	r = r.Intersect(img.Rect)
+	if r.Empty() {
+		return fmt.Errorf("The image and the rectangle don't overlap")
+	}
 
-	for y := 0; y < int(height); y++ {
-		for x := 0; x < int(width); x++ {
-			index := (int(widthBytes) * y) + (x / 8)
+	startY := r.Min.Y
+	startX := r.Min.X
+
+	for y := startY; y < r.Max.Y; y++ {
+		for x := startX; x < r.Max.X; x++ {
+			index := (int(e.widthBytes) * (y - startY)) + ((x - startX) / 8)
+
 			pixel := img.Pix[(img.Stride*y)+x]
-
-			// fmt.Printf("x=%d y=%d i=%d height=%d lim=%d\n", x, y, index, height, imageBufferSize)
-			imageBuffer[index] = imageBuffer[index] << 1
-
+			e.image[index] = e.image[index] << 1
 			if pixel <= 128 {
-				imageBuffer[index] |= 0x01
+				e.image[index] |= 0x01
 			}
 		}
 	}
 
-	_, err := e.Write([]byte{0x1D, 0x76, 0x30, 0x00, w1, w2, h1, h2})
+	return nil
+}
+
+func (e EPSPOSBitmap) Encode(w io.Writer) error {
+	w1, w2 := uint16Touint8s(e.widthBytes)
+	h1, h2 := uint16Touint8s(e.height)
+	_, err := w.Write([]byte{0x1D, 0x76, 0x30, 0x00, w1, w2, h1, h2})
 	if err != nil {
 		return err
 	}
-
-	_, err = e.Write(imageBuffer)
+	_, err = w.Write(e.image)
 	return err
+}
+
+func (e EPSPOSBitmap) Size() uint32 {
+	return (uint32(e.widthBytes) * uint32(e.height))
+}
+
+func printableSections(img image.Gray) []image.Rectangle {
+	bounds := img.Bounds()
+	if (bounds.Max.Y - bounds.Min.Y) <= 1024 {
+		return []image.Rectangle{img.Rect}
+	}
+	/* Right, otherwise we need to split this up into chunks */
+	rects := []image.Rectangle{}
+	startY := 0
+	for {
+		newY := startY + 1024
+		if newY > bounds.Max.Y {
+			newY = bounds.Max.Y
+		}
+		rects = append(rects, image.Rectangle{
+			Min: image.Point{X: bounds.Min.X, Y: startY},
+			Max: image.Point{X: bounds.Max.X, Y: newY},
+		})
+		if newY >= bounds.Max.Y {
+			break
+		}
+		startY = newY
+	}
+	return rects
+}
+
+/* */
+func (e EPSPOS) PrintImage(img image.Gray) error {
+	for _, rect := range printableSections(img) {
+		fmt.Printf("%s, printing %s\n", img.Rect, rect)
+		bitmap, err := NewEPSPOSBitmapFromGray(img, rect)
+		if err != nil {
+			return err
+		}
+		if err := bitmap.Encode(e); err != nil {
+			return err
+		}
+	}
+	return nil
 }
