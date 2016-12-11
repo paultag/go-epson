4fc313c836ac5ddbb8afda3a560ff1675ef452ac
Paul Tagliamomnte <tag@pault.ag>
2016-12-10 23:34:55 -0500

    add some barcode code

---
+++ drivers/epspos/barcode.go
@@ -0,0 +1,14 @@
+package epspos
+
+import ()
+
+func (e EPSPOS) BarcodeHeight(height uint8) error {
+	return e.write([]byte{0x1d, 0x68, height})
+}
+
+func (e EPSPOS) Barcode(data []byte) error {
+	return e.write(
+		append([]byte{0x1d, 0x6B, 0x45, byte(len(data) + 2), '*'},
+			append(data, []byte{'*', 0x00}...)...),
+	)
+}
